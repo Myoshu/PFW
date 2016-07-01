556d27f0d0cdc6e2d5f3c816611da9826320859e
Marta Jorgacevic <martakord@gmail.com>
2016-07-01 13:18:13 +0200

    postavka elemenata
    dodato:
    - player
    - osnovne varijable

--- oojs-udacity-project/js/app.js
+++ oojs-udacity-project/js/app.js
@@ -1,10 +1,7 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x,y) {
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -16,20 +13,40 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Player
+var Player = function(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-cat-girl.png';
 
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+Evul = new Enemy(0,60);
+Bug = new Enemy(60,140);
+Noms = new Enemy(30,220);
+var allEnemies = [Evul, Bug, Noms];
+
+var player = new Player(200,380);
 
 
 // This listens for key presses and sends the keys to your
