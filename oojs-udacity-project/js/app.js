78bdedf0383d75b113ae31ad547a66fe85e3dff3
Marta Jorgacevic <martakord@gmail.com>
2016-07-02 17:04:34 +0200

    moving the player

--- oojs-udacity-project/js/app.js
+++ oojs-udacity-project/js/app.js
@@ -1,7 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function(x,y) {
+var Enemy = function(x,y,speed) {
     this.x = x;
     this.y = y;
+
+    //if speed does not exists set def speed
+    //do that for the position also?
+    if(speed) {
+        this.speed = speed;
+    } else {
+        this.speed = 5;
+    }
+
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -33,17 +42,37 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
-
+Player.prototype.handleInput = function(key) {
+    switch(key) {
+        case 'up':
+            if(this.y > -30) this.y-=82;
+            if(this.y == -30) this.reset();
+        break;
+        case 'down':
+            if(this.y < 380) this.y+=82;
+        break;
+        case 'left':
+            if(this.x > 0) this.x-=100;
+        break;
+        case 'right':
+            if(this.x < 400) this.x+=100;
+        break;
+        default:
+        break;
+    }
 };
+
+Player.prototype.reset = function() {
+    this.y=380;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 Evul = new Enemy(0,60);
-Bug = new Enemy(60,140);
-Noms = new Enemy(30,220);
+Bug = new Enemy(60,140,5);
+Noms = new Enemy(30,220,5);
 var allEnemies = [Evul, Bug, Noms];
 
 var player = new Player(200,380);
