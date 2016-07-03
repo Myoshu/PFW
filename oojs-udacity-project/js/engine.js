fc4a20b7200238be154a33a354e14058eed5cb1a
Marta Jorgacevic <martakord@gmail.com>
2016-07-04 00:03:34 +0200

    choosing a character

--- oojs-udacity-project/js/engine.js
+++ oojs-udacity-project/js/engine.js
@@ -27,7 +27,8 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    var canvasDiv = document.getElementsByClassName("game-canvas")[0];
+    canvasDiv.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -172,7 +173,10 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png',
-        'images/char-cat-girl.png'
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
